e2021d4ffb11cdd6c96e28690a51ade99bee0b9f
Anastasiya <burak.67815@gmail.com>
2021-09-08 12:24:34 +0300

    simple application using enums

---
+++ OnOffView/OnOffView/Enums/SwitchStatus.swift
@@ -0,0 +1,22 @@
+//
+//  SwitchStatus.swift
+//  OnOffView
+//
+//  Created by Anastasia Burak on 1.09.21.
+//
+
+import Foundation
+
+enum SwitchStatus: Togglable {
+    
+    case on, off
+    
+    mutating func toggle() {
+        switch self {
+        case .on:
+            self = .off
+        case .off:
+            self = .on
+        }
+    }
+}
