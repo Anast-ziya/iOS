1e0ce9a13b0e91f2480cf406ca907eaff16e3426
Anastasiya <burak.67815@gmail.com>
2021-11-19 20:15:42 +0300

    Custom element in stack view

---
+++ customElement+Stack/customElement+Stack/MainView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  customElement+Stack
+//
+//  Created by Anastasia Burak on 19.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var catPhotoView: CustomView!
+    @IBOutlet weak var labelCatPhotos: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
